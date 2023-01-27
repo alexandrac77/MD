e5ed04bd956743308248d168134956af6fc11d1a
alexandrac77 <calexandra898@gmail.com>
2023-01-27 17:19:35 +0000

    synced player & camera movement, added player animations, fixed problem with decoration objects

--- GimmieChocolate/Assets/Scripts/PlayerMovement.cs
+++ GimmieChocolate/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sprite;
+    private float xDirection = 0f;
+    [SerializeField]private float speed = 4f;
+    [SerializeField]private float jumpVel = 7f;
     
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         
     }
 
     private void Update()
     {
-        float xDirection = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xDirection * 3f, rb.velocity.y); // left & right movement. if xDirection is negative we move opposite direction
+        xDirection = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xDirection * speed, rb.velocity.y); // left & right movement. if xDirection is negative we move opposite direction
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 4f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpVel);
+        }
+
+        ChangeAnimationState();
+    }
+
+    private void ChangeAnimationState()
+    {
+        // change animation state.
+        if (xDirection > 0f) // moving right.
+        {
+            animator.SetBool("isRunning", true);
+            sprite.flipX = false;
+        }
+        else if (xDirection < 0f) // moving left.
+        {
+            animator.SetBool("isRunning", true);
+            // Make character face opposite direction.
+            sprite.flipX = true;
+        }
+        else // idle
+        {
+            animator.SetBool("isRunning", false);
         }
     }
 }
